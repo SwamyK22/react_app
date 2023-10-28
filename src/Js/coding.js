d5b9260673da0dc72f4208e5ee8e767fa6d15bd3
Meghamala KM <meghamala.km@logituit.com>
2023-10-28 12:05:45 +0530

    Jc Coding

--- src/Js/coding.js
+++ src/Js/coding.js
@@ -1,39 +1,44 @@
-const ar = [2, 1, 2, 3, 4, 8];
+const ar = [2, 1, 2, 9, 3, 4, 8];
 console.log(Math.min(...ar));
-console.log(ar.findIndex((x) => x));
-const ans = ar.reduce((x,y) => {
-    const count = y % 2 === 0 ? x++ : x;
-    return x
-},0)
-console.log(ans);
+// console.log(ar.findIndex((x) => x));
 
-const dif = ar.reduce((p,c) => {
-    c > p ? p=c : p
-    return p
-},0);
-const diff = (ar) => {
-    const min = Math.min(...ar)
-    const max = Math.max(...ar)
-    return max-min
-} 
-console.log(dif);
-console.log(diff(ar));
-const a = [1, 2, 13, 2, 1, 13]
-console.log(a.filter((x,i) => x !== 13 && a[i-1] !== 13));
+// ----count even numbers in array---------------
+// const ans = ar.reduce((x, y) => {
+//     y % 2 === 0 ? x++ : x;
+//     return x
+// }, 0)
+// console.log(ans);
+
+//---find max value---
+// const dif = ar.reduce((p, c) => {
+//     c > p ? p = c : p
+//     return p
+// }, 0);
+// console.log(dif);
+
+// //max differance 
+// const diff = (ar) => {
+//     const min = Math.min(...ar)
+//     const max = Math.max(...ar)
+//     return max-min
+// } 
+// console.log(diff(ar));
+// const a = [1, 2, 13, 2, 1, 13]
+// console.log(a.filter((x,i) => x !== 13 && a[i-1] !== 13));
 
 
-function sum67(nums) {
-    const f = nums.findIndex((x) => x === 6);
-    console.log(f);
-  const l = nums.reverse().findIndex((x) => x === 7);
-  const last = nums.length - l - 1
-  const remove = nums.slice(f,last+1)
-  console.log(remove);
-}
-sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7])
+// function sum67(nums) {
+//     const f = nums.findIndex((x) => x === 6);
+//     console.log(f);
+//   const l = nums.reverse().findIndex((x) => x === 7);
+//   const last = nums.length - l - 1
+//   const remove = nums.slice(f,last+1)
+//   console.log(remove);
+// }
+// sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7])
 
-const arr = [2,6,7,7,4,6,7,1,2,3]
-const res = arr.reduce((p,c) => {
-    if(!p.includes(c)) return [...p,c]
-    return p
-},[]);
+// const arr = [2,6,7,7,4,6,7,1,2,3]
+// const res = arr.reduce((p,c) => {
+//     if(!p.includes(c)) return [...p,c]
+//     return p
+// },[]);
---
+++ src/Js/flatenArr.js
@@ -0,0 +1,104 @@
+let arr = [1, 2, 4, [[5, 6, [7, 8]]]];
+
+//----flaten arr----
+function flt(a) {
+    let result = [];
+    a.forEach((item) => {
+        if (Array.isArray(item)) {
+            result.push(...flt(item))
+        } else {
+            result.push(item)
+        }
+    })
+    return result;
+}
+console.log(flt(arr));
+
+//flaten arr using reduce fn
+function fltRdc(arr) {
+    return arr.reduce((res, item) => res.concat(Array.isArray(item) ? fltRdc(item) : item), [])
+}
+console.log(fltRdc(arr))
+
+const ar = [8, 22222, 333, 99999];
+
+function singleDigit(num) {
+    let n = 0;
+    while (num > 0) {
+        let rem = num % 10;
+        n += rem;
+        num = Math.floor(num / 10)
+    }
+    return n;
+}
+
+function convertSingleDigitArr(a) {
+    const r = []
+    a.forEach((x) => {
+        if (x.toString().length > 1) {
+            let res = singleDigit(x);
+            if (res.toString().length > 1) {
+                r.push(singleDigit(res))
+            } else {
+                r.push(res)
+            }
+        } else {
+            r.push(x)
+        }
+    });
+    return r;
+};
+
+console.log(convertSingleDigitArr(ar))
+
+function isPrime(n) {
+    let count = 0;
+    for (let i = 2; i <= n / 2; i++) {
+        if (n % i === 0) {
+            count++;
+        }
+    }
+    if (count === 0) {
+        console.log(n + " is PrimeNo")
+    }
+}
+
+
+for (let i = 1; i <= 20; i++) {
+    isPrime(i)
+}
+
+function chenchFn(arr, size) {
+    let sm = [];
+
+    for (let ele of arr) {
+        let last = sm[sm.length - 1];
+
+        if (!last || last.length === size) {
+            console.log(ele)
+            sm.push([ele])
+        } else {
+            last.push(ele)
+        }
+    }
+    return sm;
+}
+
+console.log(chenchFn([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+
+function fn(arr, size) {
+    let sm = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let last = sm[sm.length - 1];
+
+        if (!last || last.length === size) {
+            sm.push([arr[i]])
+        } else {
+            last.push(arr[i])
+        }
+    }
+    return sm
+}
+
+console.log(fn([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
